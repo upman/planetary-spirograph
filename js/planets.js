b78c8074c673aa032709b43bd5d0212b920e0a6d
Kshitij Karthick <kshitij.karthick@gmail.com>
2015-10-20 20:59:33 +0530

    Ability to randomize all parameters of planetary spirograph.
    * Random color set during execution.
    * Ability to randomize all parameters of planetary spirograph.
    * Ability to toggle between random values and default values.
    * Single event listener which listens to the event rather than reset the value during each execution.

--- js/planets.js
+++ js/planets.js
@@ -1,14 +1,56 @@
 var height = document.getElementById('container').clientHeight,
 				width = height;
 
+// All parameters that control planetary spirograph
+var parameters = {
+	"earthVelocity" : {
+		"randomFunc" : randomNumRange.bind({"start" : 5, "end" : 100}),
+		"default" : 8,
+		"new-value" : null
+	},
+	"venusVelocity" : {
+	 	"randomFunc" : randomNumRange.bind({"start" : 5, "end" : 200}),
+	 	"default" : 13,
+	 	"new-value" : null
+	},
+	"scale" : {
+		"randomFunc" : randomNumRange.bind({"start" : 1, "end" : 3}),
+		"default" : 2,
+		"new-value" : null
+	},
+	"venusOrbitRadius": {
+		"randomFunc" : randomNumRange.bind({"start" : 5, "end" : 100}),
+		"default" : 18,
+		"new-value" : null
+	},
+	"earthOrbitRadius" : {
+		"randomFunc" : randomNumRange.bind({"start" : 5, "end" : 100}),
+		"default" : 94,
+		"new-value" : null
+	},
+	"interval" : {
+		"randomFunc" : randomNumRange.bind({"start" : 25,"end" : 200}),
+		"default" : 50,
+		"new-value" : null
+	},
+	"color" : {
+		"randomFunc" : function(){return '#'+Math.floor(Math.random()*16777215).toString(16);},
+		"default" : "#e562ce", // Interesting color, but overriden
+		"new-value" : null
+	}
+};
+
+// Random color set on execution
+parameters.color.default = parameters.color.randomFunc();
+
 var tangle = new Tangle(document.getElementById('panel'), {
 	initialize: function () {
-		this.interval = 50;
-		this.scale =  2;
-		this.earthVelocity = 8;
-		this.venusVelocity = 13;
-		this.earthOrbitRadius = 94; //940 million kilometers
-		this.venusOrbitRadius = 18; ////108 million kilometers
+		this.interval = parameters.interval.default;
+		this.scale =  parameters.scale.default;
+		this.earthVelocity = parameters.earthVelocity.default;
+		this.venusVelocity = parameters.venusVelocity.default;
+		this.earthOrbitRadius = parameters.earthOrbitRadius.default; 
+		this.venusOrbitRadius = parameters.venusOrbitRadius.default;
 	},
 	update: function () {
 		this.earthRadius = 6.371 * this.scale; //6371 kilometers
@@ -17,8 +59,48 @@ var tangle = new Tangle(document.getElementById('panel'), {
 });
 
 Raphael.fn.line = function (startX, startY, endX, endY) {
-	return this.path('M' + startX + ' ' + startY + ' L' + endX + ' ' + endY);
+	var line = this.path('M' + startX + ' ' + startY + ' L' + endX + ' ' + endY);
+	// Set new value as default value for color.
+	var color = parameters.color["new-value"];
+	if(color === null){
+		// If new value does not exist use default value.
+		color = parameters.color.default;
+	}
+	// Set color to the line.
+	line.attr("stroke", color);
+	return line;
 };
+
+// Generates random number from a bound variable between range => {start, end}
+function randomNumRange(){
+	return Math.floor(Math.random() * this.end) + this.start;
+}
+
+// Event Listener added for randomizing checkbox 
+(function(){
+	document.getElementById("random-all").addEventListener("click", function(e){
+		// Iterate over all parameters generating their respective random value.
+		for(var i=0;i<Object.keys(parameters).length; i++){
+			var parameter = Object.keys(parameters)[i];
+			// Unset default value, Set random value.
+			if(parameters[parameter]["new-value"] === null){
+				var randomValue = parameters[parameter]["randomFunc"].call();
+				// color parameter is not a tangle attr
+				if(parameter !== "color"){
+					tangle.setValue(parameter, randomValue);
+				}
+				parameters[parameter]["new-value"] = randomValue;
+			}
+			// Unset random value, set default value.
+			else{
+				tangle.setValue(parameter, parameters[parameter].default);
+				parameters[parameter]["new-value"] = null;
+			}
+			// Rerender the visualization with the new updated values.  
+			reset();
+		}
+	})
+})();
 
 function calcXY(angle, radius, offsetX, offsetY) {
 
